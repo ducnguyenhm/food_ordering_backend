ad90aded033a22dcb48e443b7daa9d9f2adcbcad
ducnguyenhm <nguyenhmduc1502@gmail.com>
2024-09-07 12:46:14 -0400

    first commit

---
+++ src/routes/myUserRoute.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import MyUserController from "../controller/MyUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+
+
+const router = express.Router();
+
+// /api/my/user
+
+router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put(
+    "/",
+    jwtCheck,
+    jwtParse,
+    MyUserController.updateCurrentUser
+  );
+
+export default router;
